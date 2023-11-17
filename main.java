86528c4fa88336fbcba02f0430946e63e5902410
FlEtsv <stevefletesalfaro@MacBook-Pro-de-Steve.local>
2023-11-17 14:31:12 +0100

    metodo main a 0 a falta de convertirlo en la gui funcional

--- main.java
+++ main.java
@@ -1,15 +1,8 @@
-import java.util.Scanner;
+
 public class main {
-        public static void main(String[] args) {
-        // Ejemplo de uso
-        ConexionBD conexionBD = new ConexionBD("jdbc:mysql://localhost:3306/Formulario_CBSD", "root", "root1234");
-        Scanner nameScanner = new Scanner(System.in);
-
-        // Insertar datos en la tabla "nombre_de_la_tabla"
-        conexionBD.insertarDatos("usuarios", "joel",1, "Francisco");
-
-        // Cerrar la conexión BD
-        conexionBD.cerrarConexion();
-    }
+/**
+ * He probado con el main las clases.
+ * ya funcionan, ahora necesito crear la Gui para el formulario
+ */
 }
 
