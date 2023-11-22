ebb496e9280c8af9dd9431dd48b17e39060e642b
FlEtsv <stevefletesalfaro@MacBook-Pro.local>
2023-11-22 16:58:17 +0100

    añadido Jpanel, dentro dos jLabels y 2 Jtextfielsen las mismas lineas y en la tercera un JButton

--- mainGUI.java
+++ mainGUI.java
@@ -3,36 +3,50 @@ import javax.swing.*;
 
 public class mainGUI extends JFrame {
 
-    private JTextField pantalla; // Corregido el nombre de la clase JTextField
+    private JTextField nombreField, apellidoField;
+    private JButton boton;
 
-    private String nombre, apellido;
-
-    public mainGUI() {
+    public mainGUI(){
         // Configuración básica de la ventana
         setTitle("Formulario_CBSD");
         setSize(500, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+
+        // Crear un JPanel con fondo verde
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setBackground(new Color(200, 255, 200)); // Color de fondo verde claro
+        panelPrincipal.setLayout(new GridLayout(3, 2)); // GridLayout para organizar los componentes
 
         // Inicialización de componentes
-        pantalla = new JTextField();
-        pantalla.setEditable(false);
-        add(pantalla, BorderLayout.NORTH);
+        nombreField = new JTextField();
+        apellidoField = new JTextField();
+        boton = new JButton("Aceptar");
 
-        JButton boton = new JButton();
-        
-        boton.addActionListener(null);
+        // Configuración de colores y bordes si es necesario
+        nombreField.setBackground(Color.WHITE);
+        apellidoField.setBackground(Color.WHITE);
 
+        //Establecer tamaño preferido para nombreField
+        nombreField.setPreferredSize(new Dimension(50, 30));
 
+        // Añadir componentes al panel
+        panelPrincipal.add(new JLabel("Nombre:"));
+        panelPrincipal.add(nombreField);
+        panelPrincipal.add(new JLabel("Apellido:"));
+        panelPrincipal.add(apellidoField);
+        panelPrincipal.add(boton); // El botón se agrega en la tercera fila
+
+        // Añadir el panelPrincipal a la ventana
+        add(panelPrincipal);
 
         // Resto de la configuración de la interfaz gráfica aquí
+        // ...
 
         setVisible(true);
-        
+    }
 
     public static void main(String[] args) {
         // Puedes probar la GUI creando una instancia de MainGUI
         SwingUtilities.invokeLater(() -> new mainGUI());
     }
 }
-
